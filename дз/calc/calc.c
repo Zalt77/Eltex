13aebeb6f6f3081bf769247801e99f738ad8c1cf
Zalt77 <zaituar95@mail.ru>
2020-07-01 12:55:54 +0700

    Разнес по файлам

--- дз/calc/calc.c
+++ дз/calc/calc.c
@@ -26,32 +26,3 @@ void dev(float a, float b){
 void mul(int a, int b){
 	printf("%d\n", a*b);
 }
-
-int main()
-{
-	int a,b;
-	int sc;
-	float a1, b1;
-	do{
-	printf("\nКалькулятор\n1.Сумма\n2.Разность\n3.Деление\n4.Умножение\n5.Выход\nВыбрать из пункта меню: ");
-	scanf("%d", &sc);
-	switch(sc){
-		case 1:
-		vodchisel(&a, &b);
-		sum(a, b);
-		break;
-			case 2:
-			vodchisel(&a, &b);
-			dif(a, b);
-			break;
-				case 3:
-				dev(a1, b1);
-				break;
-					case 4:
-					vodchisel(&a, &b);
-					mul(a, b);
-					break;
-				}
-		}while(sc != 5);
-	return 0;
-}
